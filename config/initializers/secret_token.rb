0eb5f1032b3dc4203d4e45a92fd26c88d70fe8da
Christoph Gockel <github@christophgockel.de>
2014-10-09 11:07:39 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TicTacToeRails::Application.config.secret_key_base = '9471c271e248558d010b3ff6d7b0421fd8fa7ccc4b7e5f785647c76ad7f24754ca67853f772b66bc22bbb868cfcad4e6a7f84f99a108bf71a2bb4163c2f0b355'
